eac54e6e5ce36af0414c88f024c9626791c1b898
Alex <alex.lasenby@yahoo.co.nz>
2020-12-29 19:48:36 +1300

    Proabably a completely useless change, will basically be replaced right now

--- arduino/vsirgb/vsirgb.ino
+++ arduino/vsirgb/vsirgb.ino
@@ -1,6 +1,7 @@
 // Make sure this matches the rate in the 'config.py' file.
 #define BAUDRATE 9600
 #define DELAY_TIME 100
+#define DATA_LENGTH_LIMIT 512
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,11 +14,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()){
-    char data = Serial.read();
-    char str[2];
-    str[0] = data;
-    str[1] = '\0';
-    Serial.print(str);
+    char data[DATA_LENGTH_LIMIT];
+    int i = 0;
+    while (Serial.available()){
+      data[i] = Serial.read();
+      i++;
+    }
+    data[i] = '\0';
+    Serial.println(data);
   }
   delay(DELAY_TIME);
 }
